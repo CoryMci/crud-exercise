ff9c2ab7d07235694e7b7f2fd57aedc1ea62539f
CoryMci <corymciver@live.ca>
2023-03-23 11:33:18 -0700

    Add employee post route

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -6,6 +6,41 @@ exports.employee_list = function (req, res, next) {
       return next(err);
     }
     res.status(200);
+    console.log(list);
     res.json(list);
+  });
+};
+
+exports.employee_create = function (req, res, next) {
+  const employee = new Employee({
+    id: req.body.id,
+    name: req.body.name,
+    code: req.body.code,
+    profession: req.body.profession,
+    color: req.body.color,
+    branch: req.body.branch,
+    city: req.body.city,
+  });
+
+  // Check if Employee with same ID exists in database.
+  Employee.findOne({
+    id: req.body.id,
+  }).exec((err, existing_employee) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (existing_employee) {
+      res
+        .status(300)
+        .json({ message: "Employee already exists with that id!" });
+    } else {
+      employee.save((err) => {
+        if (err) {
+          return next(err);
+        }
+        res.status(200).json({ message: "success" });
+      });
+    }
   });
 };
--- server/routes/employeeRoute.js
+++ server/routes/employeeRoute.js
@@ -6,4 +6,7 @@ const employee_controller = require("../controllers/employeeController");
 //GET request for employee list
 router.get("/employees", employee_controller.employee_list);
 
+//POST request for new employee
+router.post("/employees", employee_controller.employee_create);
+
 module.exports = router;
