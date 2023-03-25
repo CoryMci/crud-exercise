95d8e1489bc648aa609fcecf65d7bae9145b8ebc
CoryMci <corymciver@live.ca>
2023-03-25 01:03:48 -0700

    Cleanup server index, convert json to sqlite

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const employees = require("./data/employees.json");
 const employeeRouter = require("./routes/employeeRoute");
 require("dotenv").config();
 
@@ -23,12 +22,5 @@ app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false })); // to allow URLencoded data
 
 app.use("/api", employeeRouter);
-
-// app.get("/api/employees", (req, res, next) => {
-//   console.log("/api/employees");
-//   res.setHeader("Content-Type", "application/json");
-//   res.status(200);
-//   res.send(JSON.stringify(employees, null, 2));
-// });
 
 app.listen(8080, () => console.log("Job Dispatch API running on port 8080!"));
